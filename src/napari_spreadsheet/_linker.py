ed41ea196520b97ba7a17ef3038d52995b240c4d
Hanjin Liu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2023-04-13 23:19:30 +0900

    fix linkers

--- src/napari_spreadsheet/_linker.py
+++ src/napari_spreadsheet/_linker.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Callable, Generic, TypeVar
 from abc import abstractmethod
 from contextlib import contextmanager
-from napari.layers import Points, Layer
+from napari.layers import Points, Shapes, Vectors, Layer
 from tabulous.color import normalize_color
 from tabulous.widgets import SpreadSheet
 from ._conversion import (
@@ -29,6 +29,10 @@ def _check_if_blocked(func: _F) -> _F:
 def get_linker(layer: Layer, sheet: SpreadSheet) -> _LayerLinker:
     if isinstance(layer, Points):
         return PointsLinker.prepare(layer, sheet)
+    elif isinstance(layer, Shapes):
+        return ShapesLinker.prepare(layer, sheet)
+    elif isinstance(layer, Vectors):
+        return VectorsLinker.prepare(layer, sheet)
     else:
         raise NotImplementedError(
             f"Linker not implemented for {type(layer).__name__} layer."
@@ -70,6 +74,15 @@ class _LayerLinker(Generic[_L]):
         df = layer_to_dataframe(self._layer)
         self._sheet.data = df
 
+    @_check_if_blocked
+    def _on_sheet_data_change(self, *_):
+        with self._sheet.events.data.blocked():
+            try:
+                spreadsheet_to_layer(self._layer, self._sheet)
+            except Exception as e:
+                self.sync_sheet()
+                raise e
+
 
 class PointsLinker(_LayerLinker[Points]):
     def link(self):
@@ -77,6 +90,7 @@ class PointsLinker(_LayerLinker[Points]):
         self._layer.events.size.connect(self._on_size_change)
         self._layer.events.face_color.connect(self._on_face_color_change)
         self._layer.events.edge_color.connect(self._on_edge_color_change)
+        self._layer.events.edge_width.connect(self._on_edge_width_change)
         self._sheet.events.data.connect(self._on_sheet_data_change)
 
     def unlink(self):
@@ -84,6 +98,7 @@ class PointsLinker(_LayerLinker[Points]):
         self._layer.events.size.disconnect(self._on_size_change)
         self._layer.events.face_color.disconnect(self._on_face_color_change)
         self._layer.events.edge_color.disconnect(self._on_edge_color_change)
+        self._layer.events.edge_width.disconnect(self._on_edge_width_change)
         self._sheet.events.data.disconnect(self._on_sheet_data_change)
 
     @_check_if_blocked
@@ -126,10 +141,91 @@ class PointsLinker(_LayerLinker[Points]):
             )
 
     @_check_if_blocked
-    def _on_sheet_data_change(self, *_):
+    def _on_edge_width_change(self, *_):
         with self._sheet.events.data.blocked():
-            try:
-                spreadsheet_to_layer(self._layer, self._sheet)
-            except Exception as e:
-                self.sync_sheet()
-                raise e
+            self._sheet.assign(edge_width=self._layer.edge_width)
+
+
+class ShapesLinker(_LayerLinker[Shapes]):
+    def link(self):
+        self._layer.events.face_color.connect(self._on_face_color_change)
+        self._layer.events.edge_color.connect(self._on_edge_color_change)
+        self._layer.events.edge_width.connect(self._on_edge_width_change)
+        self._layer.events.data.connect(self._on_data_change)
+        self._sheet.events.data.connect(self._on_sheet_data_change)
+
+    def unlink(self):
+        self._layer.events.face_color.disconnect(self._on_face_color_change)
+        self._layer.events.edge_color.disconnect(self._on_edge_color_change)
+        self._layer.events.edge_width.disconnect(self._on_edge_width_change)
+        self._layer.events.data.disconnect(self._on_data_change)
+        self._sheet.events.data.disconnect(self._on_sheet_data_change)
+
+    @_check_if_blocked
+    def _on_data_change(self, *_):
+        if self._layer.nshapes != self._sheet.index.size:
+            df = layer_to_dataframe(self._layer)
+            self._sheet.data = df
+
+    @_check_if_blocked
+    def _on_face_color_change(self, *_):
+        with self._sheet.events.data.blocked():
+            self._sheet.assign(
+                face_color=[
+                    normalize_color(x * 255).html
+                    for x in self._layer.face_color
+                ]
+            )
+
+    @_check_if_blocked
+    def _on_edge_color_change(self, *_):
+        with self._sheet.events.data.blocked():
+            self._sheet.assign(
+                edge_color=[
+                    normalize_color(x * 255).html
+                    for x in self._layer.edge_color
+                ]
+            )
+
+    @_check_if_blocked
+    def _on_edge_width_change(self, *_):
+        with self._sheet.events.data.blocked():
+            self._sheet.assign(edge_width=self._layer.edge_width)
+
+
+class VectorsLinker(_LayerLinker[Vectors]):
+    def link(self):
+        self._layer.events.edge_color.connect(self._on_edge_color_change)
+        self._layer.events.data.connect(self._on_data_change)
+        self._sheet.events.data.connect(self._on_sheet_data_change)
+
+    def unlink(self):
+        self._layer.events.edge_color.disconnect(self._on_edge_color_change)
+        self._layer.events.data.disconnect(self._on_data_change)
+        self._sheet.events.data.disconnect(self._on_sheet_data_change)
+
+    @_check_if_blocked
+    def _on_data_change(self, *_):
+        data = self._layer.data
+        if data.shape[0] != self._sheet.index.size:
+            df = layer_to_dataframe(self._layer)
+            self._sheet.data = df
+        else:
+            cols = self._sheet.columns[: self._layer.ndim * 2]
+            val = {}
+            for i in range(self._layer.ndim):
+                val[cols[i]] = data[:, 0, i].copy()
+            for i in range(self._layer.ndim):
+                val[cols[i + self._layer.ndim]] = data[:, 1, i].copy()
+            with self._sheet.events.data.blocked():
+                self._sheet.assign(val)
+
+    @_check_if_blocked
+    def _on_edge_color_change(self, *_):
+        with self._sheet.events.data.blocked():
+            self._sheet.assign(
+                edge_color=[
+                    normalize_color(x * 255).html
+                    for x in self._layer.edge_color
+                ]
+            )
--- src/napari_spreadsheet/_tests/test_conversion.py
+++ src/napari_spreadsheet/_tests/test_conversion.py
@@ -1,6 +1,7 @@
 import napari
 
 from napari_spreadsheet import MainWidget
+from numpy.testing import assert_allclose
 
 
 def test_points_conversion(make_napari_viewer):
@@ -44,3 +45,35 @@ def test_points_link_unlink(make_napari_viewer):
     assert layer.data[0, 0] == -1
     table.cell[0, 0] = -2
     assert layer.data[0, 0] == -1
+
+
+def test_shapes_link_unlink(make_napari_viewer):
+    viewer: napari.Viewer = make_napari_viewer()
+    wdt = MainWidget(viewer)
+
+    layer = viewer.add_shapes([[0, 0], [0, 1], [1, 0], [1, 1]])
+    wdt.layer_to_spreadsheet(layer)
+    table = wdt._table_viewer.current_table
+    wdt.link_spreadsheet_and_layer()
+    table.cell[0, 0] = "#FF0000"
+    assert_allclose(layer.face_color[0], [1, 0, 0, 1])
+    wdt.unlink_spreadsheet_and_layer()
+    assert_allclose(layer.face_color[0], [1, 0, 0, 1])
+    table.cell[0, 0] = "#00FF00"
+    assert_allclose(layer.face_color[0], [1, 0, 0, 1])
+
+
+def test_vectors_link_unlink(make_napari_viewer):
+    viewer: napari.Viewer = make_napari_viewer()
+    wdt = MainWidget(viewer)
+
+    layer = viewer.add_vectors([[[1, 1], [1, 1]]])
+    wdt.layer_to_spreadsheet(layer)
+    table = wdt._table_viewer.current_table
+    wdt.link_spreadsheet_and_layer()
+    table.cell[0, 0] = -1
+    assert layer.data[0, 0, 0] == -1
+    wdt.unlink_spreadsheet_and_layer()
+    assert layer.data[0, 0, 0] == -1
+    table.cell[0, 0] = -2
+    assert layer.data[0, 0, 0] == -1
